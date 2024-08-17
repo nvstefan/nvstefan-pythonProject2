5c0fef7e31bf2ed7fa413625cfc60a5bd0dbee48
nvstefan <nvstefan@mail.ru>
2024-08-17 10:32:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-number_of_completed_tasks = "всего задач 12,"
-number_of_hours_spent = "затрачено часов: 1.5,"
-Course_name = "Python,"
-time_task = 1.5 / 12
-time_for_one_task = "среднее время выполнения "
-time_name = "часа."
-print(Course_name ,number_of_completed_tasks ,number_of_hours_spent , time_for_one_task ,time_task ,time_name)
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+time_task = number_of_hours_spent / number_of_completed_tasks
+course_name = "Python,"
+print(course_name ,'всего задач: ' + str(number_of_completed_tasks) + ', затрачено часов: '+ str(number_of_hours_spent) + ', среднее время выполнения' ,time_task ,"часа.")
